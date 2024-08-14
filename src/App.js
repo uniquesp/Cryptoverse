270f4ef4553f8d7cac37cc0325ef809cb9d63e2d
uniquesp <sakshisp1342002@gmail.com>
2024-08-14 10:46:09 +0530

    fp: css added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import './App.css';
+import { Switch, Route, Link } from 'react-dom';
+import { Layout, Typography, Space } from 'antd';
+import { Navbar } from './Components/imports' 
 
 function App() {
   return (
+    <div className='app'>
+      <div className='navbar'>
+        <Navbar/>
+      </div>
 
-      <h1>cryptoverse</h1>
+      <div className='main'>
+
+      </div>
+
+      <div className='footer'>
+
+      </div>
+
+    </div>
     
   );
 }
